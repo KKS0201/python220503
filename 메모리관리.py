a6651130a6fcb374eebc393d3d413d7ce2639a40
KKS0201 <gskim0201@naver.com>
2022-05-04 10:36:38 +0900

    클래스 재정의 완성

--- 메모리관리.py
+++ 메모리관리.py
@@ -1,9 +1,11 @@
 # 메모리관리.py 
 
 class MyClass:
+    #초기화 메서드(생성자)
     def __init__(self, value):
         self.value = value 
         print("Instance is created! value=", value)
+    #소명자 메서드
     def __del__(self):
         print("Instance is deleted!")
 
