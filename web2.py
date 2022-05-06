ba20ce3f1f66a2da09b2c4bf3fdb71ed49048f15
KKS0201 <gskim0201@naver.com>
2022-05-06 13:45:31 +0900

    네이버웹툰 크롤링

--- web2.py
+++ web2.py
@@ -5,5 +5,8 @@ import urllib.request
 from bs4 import BeautifulSoup
 #블럭 주석 : ctrl + /
 # <td class="title">
-# 				<a href="/webtoon/detail?titleId=20853&no=50&weekday=fri" onclick="nclk_v2(event,'lst.title','20853','50')">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+# 				<a href="/webtoon/detail?">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
 # 						</td>
+
+
+ㅇㄹㄴㅇㄹㄴㄹ
