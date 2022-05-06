f7a463784658503434630f32e339d0cf595e4397
KKS0201 <gskim0201@naver.com>
2022-05-06 13:07:20 +0900

    웹크롤링1

---
+++ web1.py
@@ -0,0 +1,8 @@
+# web1.py
+from bs4 import BeautifulSoup
+
+#페이지 로딩
+page =open("c:\\wok\\test01.html","rt", encoding="utf-8").read()
+#검색이 용이한 객체
+soup = BeautifulSoup(page, "html.parser")
+print(soup.prettify())
