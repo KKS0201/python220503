17b0e53ea2b066d9245c54e9cce8be784621134e
KKS0201 <gskim0201@naver.com>
2022-05-06 15:11:59 +0900

    오늘의 유머 크롤링

--- 오늘의 유머.py
+++ 오늘의 유머.py
@@ -8,9 +8,9 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+for n in range(1,11):
+        #오늘의 유머 
+        data ='http://www.todayhumor.co.kr/board/list.php?table=humorbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -20,19 +20,19 @@ for n in range(0,10):
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
 
-        # <span class="subject_fixed" data-role="list-title-text" title="스타벅스 별쿠폰 1매">
-	# 스타벅스 별쿠폰 1매
-	# </span>
+# <td class="subject">
+# <a href="/board/view.php">아이 셋 키우는 아빠의 육아팁</a>
 
         #특정 태그의 속성들(attrs)
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+        list = soup.find_all('td', attrs={'class':'subject'})
 
         for item in list:
                 try:
-                        title = item.text
-                        # print(title.strip())
-                        if (re.search('애플', title)):
-                                print(title.strip())
+                        tag = item.find("a")
+                        title = tag.text
+                        print(title.strip())
+                        # if (re.search('애플', title)):
+                        #         print(title.strip())
                 except:
                         pass
         
